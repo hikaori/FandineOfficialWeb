74f823e88c94588b7e6f91cba71de370bf9bb881
kaori <kaori55194@gmail.com>
2017-12-08 10:01:40 -0800

    add

---
+++ website-fandine-china-v2-web-eng/js/navbar.js
@@ -0,0 +1,25 @@
+// <script> 
+  var navbar = {};
+  navbar.includeHTML = function(cb) {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      file = elmnt.getAttribute("include-html");
+      if (file) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+            elmnt.removeAttribute("include-html");
+            navbar.includeHTML(cb);
+          }
+        }      
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        return;
+      }
+    }
+    if (cb) cb();
+  };
+// </script>
